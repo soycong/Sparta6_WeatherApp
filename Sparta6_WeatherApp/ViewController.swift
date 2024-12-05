3c7a6cce12943f866e4b526604e4cded852a4a7a
soy <soysauce1223@naver.com>
2024-12-05 15:43:52 +0900

    Initial Commit

---
+++ Sparta6_WeatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sparta6_WeatherApp
+//
+//  Created by seohuibaek on 12/5/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Sparta6_WeatherAppTests/Sparta6_WeatherAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Sparta6_WeatherAppTests.swift
+//  Sparta6_WeatherAppTests
+//
+//  Created by seohuibaek on 12/5/24.
+//
+
+import Testing
+@testable import Sparta6_WeatherApp
+
+struct Sparta6_WeatherAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
